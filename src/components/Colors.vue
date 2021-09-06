d18c0def4766eb73eaf52213785761010ba8dba7
“noxxic3” <noxxic3@gmail.com>
2021-09-06 19:03:26 +0200

    HTTP POST request with file

--- src/components/Colors.vue
+++ src/components/Colors.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Colors</h1>
+  <h2>Colors</h2>
 
   <!--<form action="/">-->
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" v-model="userName"><br>
+    <label for="i_name">Image name:</label><br>
+    <input type="text" id="i_name" name="i_name" v-model="imageName"><br>
+
+    <label for="i_file">Image:</label><br>                                                      
+    <input type="file" id="i_file" name="i_file"  ref="file"   v-on:change="attach()">   <!--  enctype="multipart/form-data" -->
+    <br><br> 
 
     <button v-on:click="sendToBackend()">Send</button>
   <!--</form>--> 
@@ -18,60 +22,43 @@ export default {
 
   data(){
     return {
-      userName: "",
+      imageName: "",
+      image: null,
     }
   },
 
   methods: {
+    attach(){    
+      this.image = this.$refs.file.files[0];                                        // We only want to send the only file that has been attached.
+      console.log('>>>> element files array   >>>> ', this.$refs.file);             // If we see the .files property of the type="file" element, we see that it is an array that will have all the files that have been added to that element.
+      console.log('>>>> element file selected >>>> ', this.$refs.file.files[0]);
+    },
     sendToBackend(){
-      alert(this.userName);
+      alert(this.imageName);
 
-      // POST si NO enviamos archivos
-
-      let data = {                                 
-        user_name: this.userName,
-      };
+      // POST request with file
+      let formData = new FormData();                               //  ++++++++++++     // Para enviar archivos a través de AJAX, tenemos que usar un objeto de tipo FormData
+      formData.append('image_name', this.imageName);                 
+      formData.append('image', this.image);               //  ++++++++++++      <<----  Aquí le asignamos el valor del file del formulario que ahora sí lo adjuntamos y en attach() (función que creamos para escoger el file del array del elemento input type="file") cojemos el file que queremos. Antes no lo adjuntaba y se asignaba la variable  this.userImage  entera sin coger del array files el que quería.
+      console.log('>> formData >> ', formData);
 
       axios({
         method: 'post',
         url: 'images',
-        data: data,                                       
         headers: {
-        //'Authorization': 'Bearer ' + response_data_acces_token,
-        //'Content-type': 'application/json',                // No es necesario, funciona igual si no se envía. Debe ser el valor por defecto.
-        }
+          //'Authorization': 'Bearer ' + response_data_acces_token,
+          //'Content-type': 'application/json',
+          'Content-Type': 'multipart/form-data'                         //  ++++++++++++       // Para enviar archivos a través de AJAX, tenemos que poner este valor de header que equivale al valor del atributo  enctype="multipart/form-data"  que ponemos en el elemento <form> cuando queremos enviar archivos.
+        },    
+        data: formData,                                      
       })
       .then(res => {                                   
-      console.log(res);
-      console.log(res.data);
-      console.log(res.status);
+        console.log(res);
+        console.log(res.data);
+        console.log(res.status);
       })                                                  
       .catch(error => console.log(error.response));  
-
-      /*
-      // POST si enviamos archivos
-      let formData = new FormData();                               //  ++++++++++++     // Para enviar archivos a través de AJAX, tenemos que usar un objeto de tipo FormData
-      formData.append('role_name', this.userRole);                 
-      formData.append('user_name', this.userName);                 
-      formData.append('user_image', this.userImage);               //  ++++++++++++      <<----  Aquí le asignamos el valor del file del formulario que ahora sí lo adjuntamos y en attach() (función que creamos para escoger el file del array del elemento input type="file") cojemos el file que queremos. Antes no lo adjuntaba y se asignaba la variable  this.userImage  entera sin coger del array files el que quería.
-      console.log('>> formData >> ', formData);
-
-      axios({
-      method: 'post',
-      url: 'images',
-      headers: {
-        'Authorization': 'Bearer ' + response_data_acces_token,
-        //'Content-type': 'application/json',
-        'Content-Type': 'multipart/form-data'                         //  ++++++++++++       // Para enviar archivos a través de AJAX, tenemos que poner este valor de header que equivale al valor del atributo  enctype="multipart/form-data"  que ponemos en el elemento <form> cuando queremos enviar archivos.
-      },    
-      data: formData,                                      
-      })
-      .then(res => {                                   
-      console.log(res);
-      console.log(res.status);
-      })                                                  
-      .catch(error => console.log(error.response));  
-      */
+      
     }
   }
 }
